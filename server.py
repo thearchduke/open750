421c68c7de88f46f341db1865801db5a87a63e2c
Tynan Burke <tburke@Tynans-MacBook-Pro.local>
2016-07-05 12:11:08 -0700

    broke out config

--- server.py
+++ server.py
@@ -2,9 +2,11 @@
 import sys, os
 from flask import Flask
 from open750.views import open750
+import config as cf
+#import config_real as cf
 
 app = Flask(__name__)
-app.secret_key = 'flooble'
+app.secret_key = cf.secret_key
 app.register_blueprint(open750, url_prefix="/open750")
 
 app.run(debug=True)
