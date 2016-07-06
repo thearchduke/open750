f06ef8d935fa16032cc10d802133ab01bc82f113
Tynan Burke <tburke@Tynans-MacBook-Pro.local>
2016-07-06 09:44:03 -0700

    broke html safe out into config, updated config load

--- config.py
+++ config.py
@@ -4,4 +4,5 @@
 # >>> os.urandom(24)
 # '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
 ###
-secret_key = 'flooble'
+SECRET_KEY = 'flooble'
+HTML_SAFE = True
--- server.py
+++ server.py
@@ -3,17 +3,11 @@ import sys, os
 from flask import Flask
 from open750.views import open750
 
-'''
-CONFIG DETAILS:
-Put actual info in config.py instead of my mock info
-'''
-import config as cf
-
-
 #TODO: word count is inaccurate on both javascript side and python side
 #TODO: textarea is currently formatted as |safe. look up ways to make this safer!
 app = Flask(__name__)
-app.secret_key = cf.secret_key
+app.config.from_object('config')
+
 app.register_blueprint(open750, url_prefix="/open750")
 
 app.run(debug=True)
